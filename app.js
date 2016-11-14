078f87ee647c33b0a28f76a2bcfc05d3a6111007
arulajmani <arulajmani@gmail.com>
2016-11-13 20:42:48 -0500

    Added channel route to initialization

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ var app = express();
 
 mongoose.connect('mongodb://localhost:27017/gym-league');
 
-app.use(bodyParser.json({limit: '200mb'}))
-app.use(bodyParser.urlencoded({limit: '200mb', extended: true}))
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
-graph.setVersion("2.8");
 
 // Initialize all the routes
 require('./routes')(app);
